7a6d27644ec5c4cd60532368e26e14402f38acde
Imane Ramadane <ramadaneimane@gmail.com>
2026-04-24 17:12:13 +0100

    Création de projet avec les dépendances

---
+++ src/test/java/org/example/healthcare_s/HealthCareSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.healthcare_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
