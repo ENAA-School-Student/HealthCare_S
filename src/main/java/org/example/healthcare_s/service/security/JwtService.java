a16b5a9bfee65aecf1c93ae2040eb75d55a890f6
Imane <ramadaneimane8@gmail.com>
2026-05-31 02:00:11 +0100

    changement sur la fonction de generation de token avec map

--- src/main/java/org/example/healthcare_s/service/security/JwtService.java
+++ src/main/java/org/example/healthcare_s/service/security/JwtService.java
@@ -5,12 +5,18 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
 
 @Component
 public class JwtService {
@@ -20,13 +26,21 @@ public class JwtService {
 
     public String generateToken(UserDetails userDetails) {
 
+        Map<String,Object> claims= new HashMap<>();
+        List<String> roles=userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        claims.put("roles",roles);
         return Jwts.builder()
+                .setClaims(claims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+
 
     public String extractUsername(String token) {
 
@@ -65,3 +79,4 @@ public class JwtService {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 }
+
